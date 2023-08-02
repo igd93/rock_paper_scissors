f212be71fd1d31f6098e2a618cb722cb0a833e4b
igd93 <102153262+igd93@users.noreply.github.com>
2023-08-02 16:43:47 +0200

    added text

--- main.py
+++ main.py
@@ -4,4 +4,7 @@ window = tk.Tk()
 window.title("My first GUI App")
 window.geometry("1000x600")
 
+label_name = tk.Label(text = "Hello world")
+label_name.grid(column= 0, row = 0)
+
 window.mainloop()
