d0b0a9dcab07020882b283f8b62ff5c1df245fc3
igd93 <102153262+igd93@users.noreply.github.com>
2023-08-02 17:24:50 +0200

    played around with basics: Buttons, OptionMenu, Label and Entry

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 import tkinter as tk
 
-def hello(event):
-    print("Hello world")
-
 window = tk.Tk()
 window.title("My first GUI App")
 window.geometry("1000x600")
 
-mylabel = tk.Label(text = "Hello World")
-mylabel.pack(side = tk.TOP)
+opLabel = tk.Label(text = " Select the option: ")
+opLabel.grid(column = 0, row = 0)
 
+myVar = tk.StringVar(window)
+myVar.set("Select")
+opMenu = tk.OptionMenu(window, myVar, "Option1", "Option2", "Option3")
+opMenu.grid(column = 1, row = 0)
+
+def printOption():
+    value = myVar.get()
+    print(value)
+
+button = tk.Button(window, text = "Click here", command=printOption)
+button.grid(column = 0, row = 1)
 
 window.mainloop()
