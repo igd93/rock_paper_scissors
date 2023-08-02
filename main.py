6b9dfd0e1a77cd75f1e04aed458a2b5eaafffa20
igd93 <102153262+igd93@users.noreply.github.com>
2023-08-02 18:30:54 +0200

    studied radiobutons

--- main.py
+++ main.py
@@ -4,19 +4,18 @@ window = tk.Tk()
 window.title("My first GUI App")
 window.geometry("1000x600")
 
-opLabel = tk.Label(text = " Select the option: ")
-opLabel.grid(column = 0, row = 0)
+var = tk.IntVar()
+R1 = tk.Radiobutton(window, text = "Option 1", variable = var, value = 1)
+R1.grid(column = 0, row = 0)
 
-myVar = tk.StringVar(window)
-myVar.set("Select")
-opMenu = tk.OptionMenu(window, myVar, "Option1", "Option2", "Option3")
-opMenu.grid(column = 1, row = 0)
+R2 = tk.Radiobutton(window, text = "Option 2", variable=var, value = 2)
+R2.grid(column = 1, row = 0)
 
 def printOption():
-    value = myVar.get()
+    value = var.get()
     print(value)
 
-button = tk.Button(window, text = "Click here", command=printOption)
+button = tk.Button(window, text = "Click Here", command = printOption)
 button.grid(column = 0, row = 1)
 
 window.mainloop()
