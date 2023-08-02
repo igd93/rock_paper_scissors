af63bf4e5107c6b2f6590281bddb008fb003fa65
igd93 <102153262+igd93@users.noreply.github.com>
2023-08-02 17:03:09 +0200

    pack_method

--- main.py
+++ main.py
@@ -7,12 +7,8 @@ window = tk.Tk()
 window.title("My first GUI App")
 window.geometry("1000x600")
 
-label_name = tk.Label(text = "Hello world")
-label_name.grid(column= 0, row = 0)
-
-my_button = tk.Button(window, text= "some text")
-my_button.grid(column = 1, row = 0)
-my_button.bind("<Button-1>", hello)
+mylabel = tk.Label(text = "Hello World")
+mylabel.pack(side = tk.TOP)
 
 
 window.mainloop()
