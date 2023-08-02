daa753ec20a8e7e0925103309bde0e7a2ddedc61
igd93 <102153262+igd93@users.noreply.github.com>
2023-08-02 18:40:39 +0200

    checklist

--- main.py
+++ main.py
@@ -4,18 +4,28 @@ window = tk.Tk()
 window.title("My first GUI App")
 window.geometry("1000x600")
 
-var = tk.IntVar()
-R1 = tk.Radiobutton(window, text = "Option 1", variable = var, value = 1)
-R1.grid(column = 0, row = 0)
+myLabel = tk.Label(text = "Select your hobbies: ")
+myLabel.grid(column = 0, row = 0)
 
-R2 = tk.Radiobutton(window, text = "Option 2", variable=var, value = 2)
-R2.grid(column = 1, row = 0)
+CheckVar1 = tk.IntVar()
+CheckVar2 = tk.IntVar()
+C1 = tk.Checkbutton(window, text = "Singing", 
+                    variable = CheckVar1, onvalue=1, offvalue=0,
+                    height=2, width=10)
+C1.grid(column= 0 ,row = 1)
 
-def printOption():
-    value = var.get()
-    print(value)
+C2 = tk.Checkbutton(window, text = "Reading", 
+                    variable = CheckVar2, onvalue=1, offvalue=0,
+                    height=2, width=10)
+C2.grid(column= 0 ,row = 2)
 
-button = tk.Button(window, text = "Click Here", command = printOption)
-button.grid(column = 0, row = 1)
+def printCheck():
+    value1 = CheckVar1.get()
+    value2 = CheckVar2.get()
+    print(value1)
+    print(value2)
+
+button = tk.Button(window, text = "Submit", command = printCheck)
+button.grid(column = 0, row = 3)
 
 window.mainloop()
