7ebe2e3efc629d77a23e432b670f27fe42d8f167
igd93 <102153262+igd93@users.noreply.github.com>
2023-08-02 16:58:16 +0200

    added binding to a button

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import tkinter as tk
 
+def hello(event):
+    print("Hello world")
+
 window = tk.Tk()
 window.title("My first GUI App")
 window.geometry("1000x600")
@@ -7,7 +10,9 @@ window.geometry("1000x600")
 label_name = tk.Label(text = "Hello world")
 label_name.grid(column= 0, row = 0)
 
-button_name = tk.Button(window, text= "some text")
-button_name.grid(column = 1, row = 0)
+my_button = tk.Button(window, text= "some text")
+my_button.grid(column = 1, row = 0)
+my_button.bind("<Button-1>", hello)
+
 
 window.mainloop()
