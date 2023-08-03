618e97548b67527cc674f9054306add9ce350bbb
igd93 <102153262+igd93@users.noreply.github.com>
2023-08-03 17:31:44 +0200

    an example of text widget

--- main.py
+++ main.py
@@ -4,28 +4,16 @@ window = tk.Tk()
 window.title("My first GUI App")
 window.geometry("1000x600")
 
-myLabel = tk.Label(text = "Select your hobbies: ")
+myLabel = tk.Label(window, text="Enter your address: ")
 myLabel.grid(column = 0, row = 0)
+myText = tk.Text(window, height = 10, width = 20)
+myText.grid(column = 1, row = 0)
 
-CheckVar1 = tk.IntVar()
-CheckVar2 = tk.IntVar()
-C1 = tk.Checkbutton(window, text = "Singing", 
-                    variable = CheckVar1, onvalue=1, offvalue=0,
-                    height=2, width=10)
-C1.grid(column= 0 ,row = 1)
+def inputData():
+    address = myText.get("1.0", "end")
+    print(address)
 
-C2 = tk.Checkbutton(window, text = "Reading", 
-                    variable = CheckVar2, onvalue=1, offvalue=0,
-                    height=2, width=10)
-C2.grid(column= 0 ,row = 2)
-
-def printCheck():
-    value1 = CheckVar1.get()
-    value2 = CheckVar2.get()
-    print(value1)
-    print(value2)
-
-button = tk.Button(window, text = "Submit", command = printCheck)
-button.grid(column = 0, row = 3)
+myButton = tk.Button(window, text = "Submit", command = inputData)
+myButton.grid(column= 0, row = 1)
 
 window.mainloop()
